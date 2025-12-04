beb33f00b403e53428439a5fe05d1fcc2c7c919a
alexander-p1 <alexanderpettersson_@hotmail.com>
2025-12-04 15:08:54 +0100

    Added configs and dependencies

--- strajk-bowling/vite.config.js
+++ strajk-bowling/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setupFiles.js",
+    coverage: {
+      provider: "v8",
+      threshold: {
+        statements: 90,
+        branches: 90,
+        functions: 90,
+        lines: 90
+      }
+    }
+  }
 })
