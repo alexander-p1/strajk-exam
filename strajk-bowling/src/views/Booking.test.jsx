a18df4b7b9ee1e894a0766a61b34362c904c03a8
alexander-p1 <alexanderpettersson_@hotmail.com>
2025-12-09 11:44:16 +0100

    commit

---
+++ strajk-bowling/src/views/Booking.test.jsx
@@ -0,0 +1,10 @@
+import { fireEvent, screen, render } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import { MemoryRouter } from "react-router-dom";
+import Booking from "./Booking.jsx"
+
+describe("BookingTest", () => {
+  it("Should be able to book date, time and players and to reserve 1 or more lanes", () => {
+    
+  })
+})
